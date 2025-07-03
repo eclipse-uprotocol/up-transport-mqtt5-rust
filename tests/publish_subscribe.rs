b3357c3ebf32b5c9ff5566e3ed96e18151d975fc
Kai Hudalla <kai.hudalla@bosch.com>
2025-07-03 17:25:54 +0200

    Add integration tests verifying reconnect behavior of the MQTT client
    Adapted check CI job to use standard cargo test instead of nextest
    because the latter does not support the test-serial crate which is
    needed for running the integration tests sequentially so that the
    Mosquitto container can be run with a fixed host port. This is
    necessary because the container is being stopped and restarted as
    part of the test scenarios.

--- tests/publish_subscribe.rs
+++ tests/publish_subscribe.rs
@@ -13,63 +13,131 @@
 
 use std::{str::FromStr, sync::Arc, time::Duration};
 
+use log::debug;
+use serial_test::serial;
 use up_rust::{MockUListener, UMessageBuilder, UTransport, UUri};
 
 mod common;
 
+const MOSQUITTO_CONFIG_W_PERSISTENCE: &str = r#"
+persistence true
+persistence_location /mosquitto/data/
+"#;
+
+#[test_case::test_case(Some(MOSQUITTO_CONFIG_W_PERSISTENCE); "for Mosquitto with persistence")]
+#[test_case::test_case(None; "for Mosquitto without persistence")]
 #[tokio::test]
+#[serial]
 #[cfg_attr(not(docker_available), ignore)]
 // This test requires Docker to run the Mosquitto MQTT broker.
-async fn test_publish_and_subscribe() {
-    env_logger::init();
+async fn test_publish_and_subscribe_succeeds_after_reconnect(mosquitto_config: Option<&str>) {
+    let _ = env_logger::try_init();
 
     // fixture
-    let (_mosquitto, broker_port) = common::start_mosquitto().await;
-    let message_received = Arc::new(tokio::sync::Notify::new());
-
-    let source = UUri::from_str("//Publisher/A8000/2/8A50").expect("Failed to create source URI");
-    let message_to_send = UMessageBuilder::publish(source)
+    let mosquitto = common::start_mosquitto(mosquitto_config, Some(15000)).await;
+    let topic = UUri::from_str("//publisher/A8000/2/8A50").expect("invalid topic URI");
+    let message_to_send = UMessageBuilder::publish(topic)
         .build_with_payload(
             "test_payload",
             up_rust::UPayloadFormat::UPAYLOAD_FORMAT_TEXT,
         )
-        .expect("Failed to create message");
-    let expected_message = message_to_send.clone();
-    let message_received_clone = message_received.clone();
+        .expect("Failed to build message");
+    let cloned_message = message_to_send.clone();
+
+    let message_received = Arc::new(tokio::sync::Notify::new());
+    let message_received_barrier = message_received.clone();
     let mut listener = MockUListener::new();
-    listener.expect_on_receive().once().return_once(move |msg| {
-        assert_eq!(msg, expected_message);
-        message_received_clone.notify_one();
+    listener.expect_on_receive().returning(move |msg| {
+        debug!("handling message in listener");
+        assert_eq!(msg, cloned_message);
+        message_received.notify_one();
     });
 
-    let subscriber = common::create_up_transport_mqtt("Subscriber", broker_port)
-        .await
-        .expect("failed to create transport at receiving end");
+    let subscriber = common::create_up_transport_mqtt(
+        "subscriber",
+        mosquitto.port(),
+        Some("subscriber_client_id"),
+    )
+    .await
+    .map(Arc::new)
+    .expect("failed to create transport at receiving end");
     subscriber
         .connect()
         .await
         .expect("failed to connect subscriber to broker");
     let source_filter =
-        UUri::from_str("//Publisher/A8000/2/FFFF").expect("Failed to create source filter");
+        UUri::from_str("//publisher/A8000/2/FFFF").expect("Failed to create source filter");
     subscriber
         .register_listener(&source_filter, None, Arc::new(listener))
         .await
         .expect("failed to register listener");
 
-    let publisher = common::create_up_transport_mqtt("Publisher", broker_port)
-        .await
-        .expect("failed to create transport at sending end");
+    let publisher = common::create_up_transport_mqtt(
+        "publisher",
+        mosquitto.port(),
+        Some("publisher_client_id"),
+    )
+    .await
+    .map(Arc::new)
+    .expect("failed to create transport at sending end");
     publisher
         .connect()
         .await
         .expect("failed to connect publisher to broker");
 
+    // verify that a message sent by the publisher is received by the subscriber
     publisher
-        .send(message_to_send)
+        .send(message_to_send.clone())
         .await
         .expect("failed to publish message");
 
-    tokio::time::timeout(Duration::from_millis(1000), message_received.notified())
+    assert!(
+        tokio::time::timeout(
+            Duration::from_millis(1000),
+            message_received_barrier.notified()
+        )
         .await
-        .expect("did not receive published message before timeout");
+        .is_ok(),
+        "did not receive message before timeout"
+    );
+
+    // now restart the broker to simulate a (temporary) disconnect
+    mosquitto.stop().await.expect("Failed to stop Mosquitto");
+    debug!("stopped Mosquitto, waiting for 1 second before restarting");
+    tokio::time::sleep(Duration::from_secs(1)).await;
+    debug!("restarting Mosquitto");
+    mosquitto
+        .start()
+        .await
+        .expect("Failed to start Mosquitto again");
+    debug!("Mosquitto restarted");
+
+    // wait for the connections and subscriptions to be reestablished
+    tokio::time::timeout(
+        Duration::from_secs(10),
+        futures::future::join(
+            common::connection_established(publisher.clone()),
+            common::connection_established(subscriber.clone()),
+        ),
+    )
+    .await
+    .expect("transports did not reconnect in time");
+
+    // publish the message again
+    publisher
+        .send(message_to_send)
+        .await
+        .expect("failed to publish message after reconnect");
+
+    // [utest->req~up-transport-mqtt5-reconnection~1]
+    // wait for the message to be received again
+    assert!(
+        tokio::time::timeout(
+            Duration::from_millis(1000),
+            message_received_barrier.notified()
+        )
+        .await
+        .is_ok(),
+        "did not receive second message before timeout"
+    );
 }
